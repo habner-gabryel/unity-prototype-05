90a779728ef66c9f282da7f2d2739e2024d93722
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-23 19:29:20 -0300

    5.1.6 commit

---
+++ unity-prototype-05/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget(){
+        while(true){
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
