dfcf2a88b8bce14ceb2f915eb4359ee2dae048f4
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-23 20:44:38 -0300

    5.3.6 commit

--- unity-prototype-05/Assets/Scripts/GameManager.cs
+++ unity-prototype-05/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -30,6 +31,10 @@ public class GameManager : MonoBehaviour
     public void GameOver(){
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+    }
+
+    public void RestartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void UpdateScore(int scoreToAdd){
