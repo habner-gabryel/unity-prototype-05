3cc67eb5ae911224862b48c92313d723f14ad7ec
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-23 20:10:06 -0300

    5.2.3 commit

--- unity-prototype-05/Assets/Scripts/Target.cs
+++ unity-prototype-05/Assets/Scripts/Target.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
     private float minSpeed = 12;
