0b57251ce0c8d71cfb3bf4131cee351f6575f5ff
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-23 21:43:06 -0300

    5.5.5 commit

--- challenge-05/Assets/Challenge 5/Scripts/GameManagerX.cs
+++ challenge-05/Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -23,13 +23,13 @@ public class GameManagerX : MonoBehaviour
     private float minValueY = -3.75f; //  y value of the center of the bottom-most square
     
     // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
-    public void StartGame()
+    public void StartGame(int difficulty)
     {
-        spawnRate /= 5;
+        spawnRate /= difficulty;
         isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
-        UpdateScore(0);
+        UpdateScore(score);
         titleScreen.SetActive(false);
     }
 
@@ -70,7 +70,7 @@ public class GameManagerX : MonoBehaviour
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "score";
+        scoreText.text = "Score: " + score;
     }
 
     // Stop game, bring up game over text and restart button
